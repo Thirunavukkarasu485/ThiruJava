00e7bee49fe46f0fc2257ef2aac2ddd32c408764
Thirunavukkarasu485 <thirun948@gmail.com>
2025-10-29 19:07:26 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.day3;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("This is the line from github");
+//        System.err.println("Error Message!");
+        System.out.println("This is the last line");
+        String name="Thirunavukkarasu";
+        int age=19;
+        System.out.printf("My Name is %s%nMy Age is %d",name,age);
+    }
+}
